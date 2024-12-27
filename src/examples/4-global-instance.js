c8a3d4b74a654c635f4e94a408aeb7095a93d180
talasu-sahith <sahithtalasu.123@gmail.com>
2024-12-27 14:15:14 +0530

    global header instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
+import "../axios/axios";
 
-const productsUrl = 'https://www.course-api.com/react-store-products';
-const randomUserUrl = 'https://randomuser.me/api';
+const productsUrl = "https://www.course-api.com/react-store-products";
+const randomUserUrl = "https://randomuser.me/api";
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const resp1 = await axios.get(productsUrl);
+      const resp2 = await axios.get(randomUserUrl);
+      console.log(resp1.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+    console.log("global axios instance");
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
